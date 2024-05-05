f93548529c356b0b09b256297874b10ac2e86e45
mohamedesmat1997 <158211063+63456356@users.noreply.github.com>
2024-05-05 12:44:40 +0300

    get current

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\User;
 
 
 class ProjectController extends Controller
@@ -16,6 +17,8 @@ class ProjectController extends Controller
         return response()->json($projects);
     }
 
+
+
     // Get a specific project by ID
     public function show($id)
     {
@@ -26,6 +29,8 @@ class ProjectController extends Controller
         return response()->json($project);
     }
 
+
+
     // Create a new project
     public function store(Request $request)
     {
@@ -33,16 +38,60 @@ class ProjectController extends Controller
         return response()->json($project, 201);
     }
 
-    // Update an existing project
-    public function update(Request $request, $id)
+  
+
+
+    
+    public function getCurrentProjectById($id)
     {
-        $project = Project::find($id);
-        if (!$project) {
-            return response()->json(['error' => 'Project not found'], 404);
+
+    
+       // Retrieve projects associated with the given user ID and current status
+       $projects = Project::where('user_id', $id)
+       ->where('status', 'current')
+       ->get();
+
+        if ($projects) {
+            return response()->json($projects, 200);
+        } else {
+            return response()->json(['message' => 'Project not found'], 404);
         }
-        $project->update($request->all());
-        return response()->json($project);
     }
+
+
+   
+
+
+    public function update(Request $request)
+    {
+        // Retrieve the data from the request
+        $requestData = $request->all();
+
+        // Find the project to update based on some criteria (e.g., project name)
+        $project = Project::where('id', $requestData['id'])->first();
+
+        if ($project) {
+            // Update the project with the new data
+            $project->update($requestData);
+
+            return response()->json(['message' => 'Project updated successfully'], 200);
+        } else {
+            return response()->json(['message' => 'Project not found'], 404);
+        }
+    }
+
+
+    public function select($user_id)
+    {
+        // Retrieve projects associated with the given user ID
+        $projects = Project::where('user_id', $user_id)->get();
+
+        // Return the projects as JSON response
+        return response()->json($projects);
+    }
+
+
+    
 
     // Delete a project
     public function destroy($id)
@@ -54,5 +103,6 @@ class ProjectController extends Controller
         $project->delete();
         return response()->json(['message' => 'Project deleted']);
     }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,10 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
 
+
+
+
+
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{id}', [UserController::class, 'show']);
 Route::post('/users', [UserController::class, 'store']);
@@ -48,7 +52,9 @@ Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 
 Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/current-project/{id}', [ProjectController::class, 'getCurrentProjectById']);
 Route::get('/projects/{id}', [ProjectController::class, 'show']);
+Route::get('/projects/select/{user_id}', [ProjectController::class, 'select']);
 Route::post('/projects', [ProjectController::class, 'store']);
-Route::put('/projects/{id}', [ProjectController::class, 'update']);
+Route::put('/projects', [ProjectController::class, 'update']);
 Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
